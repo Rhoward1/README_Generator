785914687942c1c2bad7472879462d993063aabf
Rhoward1 <ryanmhoward@gmail.com>
2022-04-13 01:04:08 -0500

    worked on function to catch data from questions and functions to display badges

--- Develop/index.js
+++ Develop/index.js
@@ -1,42 +1,49 @@
+// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+
+// TODO: Create an array of questions for user input
+
 
 inquirer
-  .prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?"
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "Describe the project?"
-      },
-      {
-        type: "input",
-        name: "installationInfo",
-        message: "What are the installation instructions?"
-      },
-      {
-        type: "input",
-        name: "usageInfo",
-        message: "What is the usage information?"
-      },
-      {
-        type: "input",
-        name: "contributionGuidelines",
-        message: "What are the Contribution Guidelines?"
-      },
-      {
-        type: "input",
-        name: "testInstructions",
-        message: "What are the test instructions?"
-    },
-      {
-        type: "list",
-        name: "license",
-        message: "What kind of license should your project have?",
-        choices: ["MIT","placeholder","None"]
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Describe the project?"
+        },
+        {
+            type: "input",
+            name: "installationInfo",
+            message: "What are the installation instructions?"
+        },
+        {
+            type: "input",
+            name: "usageInfo",
+            message: "What is the usage information?"
+        },
+        {
+            type: "input",
+            name: "testInstructions",
+            message: "What are the test instructions?"
+        },
+        {
+            type: "input",
+            name: "contributionGuidelines",
+            message: "What are the Contribution Guidelines?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What kind of license should your project have?",
+            choices: ["MIT License", "The Unlicense", "None"]
 
         },
         {
@@ -48,30 +55,35 @@ inquirer
             type: "input",
             name: "email",
             message: "What is your email address?"
-          },
+        },
 
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+        /* Pass your questions in here */
+    ])
     
+    .then((answers) => { 
+        console.log(answers); 
+        const markdown = generateMarkdown(answers);
+        writeToFile("README.md", markdown);
+    })
+
+        // Use user feedback for... whatever!!
+    
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+
+
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
