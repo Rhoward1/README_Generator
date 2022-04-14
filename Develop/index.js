708b55344ab5b2248076479ed6371aac1eb1440a
Rhoward1 <ryanmhoward@gmail.com>
2022-04-14 00:51:16 -0500

    changed markdown layout and attemped generating an image

--- Develop/index.js
+++ Develop/index.js
@@ -8,73 +8,78 @@ const generate = require('./utils/generateMarkdown');
 
 // const promptQuestions = () => {
 
- const questions = [   
-        {
-            type: "input",
-            name: "title",
-            message: "What is the title of your project?"
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Describe the project?"
-        },
-        {
-            type: "input",
-            name: "installationInfo",
-            message: "What are the installation instructions?"
-        },
-        {
-            type: "input",
-            name: "usageInfo",
-            message: "What is the usage information?"
-        },
-        {
-            type: "input",
-            name: "testInstructions",
-            message: "What are the test instructions?"
-        },
-        {
-            type: "input",
-            name: "contributionGuidelines",
-            message: "Were there any contributers?"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "What kind of license should your project have?",
-            choices: ["MIT License", "The Unlicense", "None"]
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe the project?"
+    },
+    {
+        type: "input",
+        name: "installationInfo",
+        message: "What are the installation instructions?"
+    },
+    {
+        type: "input",
+        name: "usageInfo",
+        message: "What is the usage information?"
+    },
+    {
+        type: "input",
+        name: "image",
+        message: "What is the filename of your image?",
+    },
+    {
+        type: "input",
+        name: "testInstructions",
+        message: "What are the test instructions?"
+    },
+    {
+        type: "input",
+        name: "contributionGuidelines",
+        message: "Were there any contributers?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT License", "The Unlicense", "None"]
 
-        },
-        {
-            type: "input",
-            name: "gitHubUsername",
-            message: "What is your GitHub username?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your email address?"
-        },
+    },
+    {
+        type: "input",
+        name: "gitHubUsername",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
 
-        /* Pass your questions in here */
-    ];
-    
+    /* Pass your questions in here */
+];
 
-    
+
+
 // TODO: Create a function to write README file
-function writeFile(fileName, data) { 
-    fs.writeFile(fileName,data,(err) =>
-    err ? console.log(err) : console.log('Successfully created README.md'));
+function writeFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md'));
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt(questions)
-    .then((answers) => writeFile("README.md", generate(answers)))
-    .catch((err) => console.log(err));
- }
+        .prompt(questions)
+        .then((answers) => writeFile("README.md", generate(answers)))
+        .catch((err) => console.log(err));
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,63 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
-// if (license === "None") {
-//   return "";
-// }
+function renderLicenseBadge(license) { 
+if (license === "None") {
+    return "No License";
+}
 
+if (license === "MIT License") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
-// if (license === "MIT License") {
-//   return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-// }
-// else if (license === "The Unlicense") {
-//   return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
-
-
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
+if (license === "The Unlicense") {
+    return `[![License: Unlicense](https://img.shields.io/badge/License-Unlicense-blue.svg)](http://unlicense.org/)`
 
 }
+}
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) { }
+
+
+
+// // // TODO: Create a function that returns the license section of README
+// // // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.title}
 
-
-${data.license}
-## Table of Contents
-*[Description](#description)
-*[Installation Info](#installationInfo)
-*[Usage Information](#usageInfo)
-*[Test Instructions](#testInstructions)
-*[Contributors](#contributionGuidelines)
-
-*[Contact Me](#Contact-Information)
-  
-## Description
-${data.description}
-  
-## Installation
-${data.installationInfo}
-  
-## Usage
-${data.usageInfo}
 ## License
-${data.license}
-## Contributors
-${data.contributionGuidelines}
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+- [Project Description](#description)  
+- [Installation Information](#installationInfo)  
+- [Usage Information](#usageInfo)  
+- [Test Instructions](#testInstructions)  
+- [Contributors on this Project](#contributionGuidelines)  
+- [Contact Me](#Contact-Information)  
   
-## How to Test
-${data.testInfo}
+## Project Description
+- ${data.description}
+  
+## Installation Information
+- ${data.installationInfo}
+  
+## Usage Information
+- ${data.usageInfo}
+- ![](./assets/${data.image})
+
+## Contributors on this Project
+- ${data.contributionGuidelines}
+- *[Contribution Guidlines](https://github.com/github/docs/blob/main/CONTRIBUTING.md)*
+  
+## Test Instructions
+- ${data.testInstructions}
   
 ## Contact-Information
-[Github Profile](https://github.com/${data.username})
-${data.email}
+- Contact me on Github [${data.gitHubUsername}](https://github.com/${data.gitHubUsername})
+  - Email at ${data.email}
 `;
 }
 
